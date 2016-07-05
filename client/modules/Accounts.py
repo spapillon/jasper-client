f22535cccc5fd1293bcf9ef5d7cb2feb7137850a
Simon Papillon <papillonsimon@gmail.com>
2016-07-05 17:31:34 +0000

    Match profile file to input, error if not found

--- client/modules/Accounts.py
+++ client/modules/Accounts.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8-*-
 import re
-import os
-import sys
-import shutil
-import logging
-import signal
 import yaml
-import argparse
-
-from client import tts, stt, jasperpath, diagnose
+import os 
+from client import jasperpath
 from client.conversation import Conversation
 
 
@@ -16,17 +10,24 @@ WORDS = ["LOG", "IN", "OUT", "EXIT", "SIGN", "OFF", "BYE"]
 
 
 def handle(text, mic, profile):
-	if text == "login":
-		new_configfile = jasperpath.config('profile2.yml')
-		with open(new_configfile, "r") as f:
-			config = yaml.safe_load(f)
-		conversation = Conversation()
-        	conversation.setProfile("JASPER", mic, config)
+	match = re.search('(?<=login ).*', text)
+	if match is not None:
+		user = match.group()
+		new_configfile = jasperpath.config(user + '.yml')
+		if os.path.exists(new_configfile):
+			with open(new_configfile, "r") as f:
+				config = yaml.safe_load(f)
+			conversation = Conversation()
+        		conversation.setProfile("JASPER", mic, config)
+			mic.say("Welcome " + user)
+		else:
+			mic.say("Error, user not found")
 	elif text == "logout":
 		new_configfile = jasperpath.config('configs.yml')
 		with open(new_configfile, "r") as f:
 			config = yaml.safe_load(f)
 		conversation = Conversation()
 		conversation.setProfile("JASPER", mic, config)
+		mic.say("Bye" + profile['first_name'])
 def isValid(text):
 	return bool(re.search(r'(login|logout)', text, re.IGNORECASE))
